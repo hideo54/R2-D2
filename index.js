7e68582a4e96f275a321ae442e9175f08b32e1f6
hideo54 <hideo54@hideo54.com>
2016-02-18 04:02:57 +0900

    Creation of basic server and client

---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http'),
+    jade = require('jade');
+
+http.createServer(function (req, res) {
+    if (req.method == 'GET') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        var f = jade.compileFile('index.jade', {
+            'pretty': true
+        });
+        res.end(f());
+    } else if (req.method == 'POST') {
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+        });
+        req.on('end', function () {
+            // Do something:
+            console.log(body);
+        });
+    }
+}).listen(3000, 'localhost');
